aa552d7ba835b7433648956f991d18f271ddf676
THuy Chi <chi.hathuy15@gmail.com>
2024-09-18 16:11:48 +0700

    feat: sign in, sign up api

---
+++ src/users/users.service.ts
@@ -0,0 +1,31 @@
+import { ConflictException, Injectable, NotAcceptableException } from '@nestjs/common';
+import { SignUpDTO } from '../auth/dto/sign-up.dto';
+import { UserRepository } from 'src/repository/user.repository';
+
+@Injectable()
+export class UsersService {
+  constructor(private userRepository: UserRepository) {}
+  async findOneByEmail(email: string) {
+    const foundUser = this.userRepository.findOneByEmail(email);
+    if (!foundUser) {
+      throw new NotAcceptableException(`${email} does not exist.`)
+    };
+    return {success: true, message: foundUser};
+  }
+
+  async createUSer(email: string, password: string, name: string, phone: string) {
+    const foundUser = this.userRepository.findOneByEmail(email);
+    if (foundUser) {
+      throw new ConflictException("Email is already existed.")
+    }
+    const createdUser = this.userRepository.createUser(email, password, name, phone);
+    if (!createdUser) {
+      throw new ConflictException("Cannot create new user.")
+    }
+    return {success: true, message: createdUser};
+  }
+
+  // remove(id: number) {
+  //   return `This action removes a #${id} user`;
+  // }
+}
