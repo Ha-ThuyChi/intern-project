3ab567f6d38be744f3fe53dc66513ab6f35181fa
THuy Chi <chi.hathuy15@gmail.com>
2024-09-23 16:10:19 +0700

    feat: fix 10K+ changes

--- src/server/prisma/prisma.service.ts
+++
@@ -1,5 +0,0 @@
-import { Injectable } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client';
-
-@Injectable()
-export class PrismaService extends PrismaClient {} // to make the service have access to client and then db
