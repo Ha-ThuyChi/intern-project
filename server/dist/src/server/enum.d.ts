5fa7d7abebb50ebdf2b99f708776a0124110a862
THuy Chi <chi.hathuy15@gmail.com>
2024-09-24 14:53:19 +0700

    <commit message>

--- server/dist/src/server/enum.d.ts
+++
@@ -1,9 +0,0 @@
-export declare enum LocationType {
-    ONLINE = "ONLINE",
-    OFFLINE = "OFFLINE"
-}
-export declare enum TicketType {
-    PAID = "PAID",
-    DONATION = "DONATION",
-    FREE = "FREE"
-}
